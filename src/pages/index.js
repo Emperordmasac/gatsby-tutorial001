c678b50dcd0a6061ac747dbdc568d440956a0648
Abiola Olamilekan <abiolawalter2015@gmail.com>
2022-07-08 18:22:18 +0100

    added gatsby plugins

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
     return (
@@ -8,6 +9,7 @@ const IndexPage = () => {
             content={`I'm making this by following the Gatsby Tutorial.`}
         >
             <h1>Welcome to my Gatsby site!</h1>
+            <StaticImage src="../images/Baby.png" alt="bla bla bla" />
         </Layout>
     );
 };
